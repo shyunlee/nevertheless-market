98c346cd96e2b2ebe12fb8457758dcfe56ab3c63
shyunlee <shyunlee0909@gmail.com>
2025-04-29 00:01:39 -0500

    SMS login UI page

---
+++ app/sms/page.tsx
@@ -0,0 +1,18 @@
+import FormInput from "@/components/FormInput";
+import FormButton from "@/components/FormButton";
+
+export default function SMSLogin() {
+  return (
+    <section className='flex flex-col gap-8 py-10 min-h-screen justify-center'>
+      <div className='flex flex-col gap-2 *:font-medium'>
+        <h1 className='text-2xl'>SMS Login</h1>
+        <h2 className='text-lg'>Verify your phone number</h2>
+      </div>
+      <form className='flex flex-col gap-4'>
+        <FormInput type='number' placeholder="Phone Number" required />
+        <FormInput type='number' placeholder="Verfication Code" required />
+        <FormButton text='Verify' isLoading={false} />
+      </form>
+    </section>
+  )
+};
