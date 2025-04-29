ad582e69f47fc5be5883a0c74dad0c62668ad04f
shyunlee <shyunlee0909@gmail.com>
2025-04-28 23:21:52 -0500

    add component social login and implement UI for login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,18 +2,16 @@ import FormInput from "@/components/FormInput";
 import FormButton from "@/components/FormButton";
 import SocialLogin from "@/components/SocialLogin";
 
-export default function SignUpPage() {
+export default function LogInPage() {
   return (
     <section className='flex flex-col gap-8 py-10 min-h-screen justify-center'>
       <div className='flex flex-col gap-2 *:font-medium'>
         <h1 className='text-2xl'>Hello, Welcome!</h1>
-        <h2 className='text-xl'>Please fill in the form below to join</h2>
+        <h2 className='text-lg'>Please login with your email and password</h2>
       </div>
       <form className='flex flex-col gap-4'>
-        <FormInput type='text' placeholder="Username" required />
         <FormInput type='email' placeholder="Email" required />
         <FormInput type='password' placeholder="Password" required />
-        <FormInput type='password' placeholder="Confirm Password" required />
         <FormButton text='Create Account' isLoading={false} />
       </form>
       <SocialLogin />
