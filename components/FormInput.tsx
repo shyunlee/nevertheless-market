68cb556ad1fb43986bf2eea95fd4ca0be98849ef
shyunlee <shyunlee0909@gmail.com>
2025-05-02 00:04:06 -0500

    refactoring FormInput props

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,24 +1,21 @@
+import { InputHTMLAttributes } from "react";
+
 type FormInputProps = {
-  type: string;
-  placeholder?: string;
-  required?: boolean;
-  errors?: string[];
   name: string;
   value: string;
+  errors?: string[];
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function FormInput({type, placeholder, required=false, errors=[], name, value, onChange}: FormInputProps) {
+export default function FormInput({errors=[], name, value, onChange, ...rest}: FormInputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className='flex flex-col gap-2'>
       <input
         className='w-full text-white bg-transparent border-none focus:outline-none ring-1 ring-neutral-200 rounded-md py-2 px-3 focus:ring-2 focus:ring-orange-500'
-        type={type}
-        placeholder={placeholder}
-        required={required}
         name={name}
         onChange={onChange}
         value={value}
+        {...rest}
       />
       {
         errors.length > 0 && errors.map((errorText, index) => (
