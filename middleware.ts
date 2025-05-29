c1758ec4e29b06ee2a60f79baf78b74c85285126
shyunlee <shyunlee0909@gmail.com>
2025-05-28 23:40:13 -0500

    implement middleware

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean
+}
+
+const publicUrlOnly: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "create-account": true
+}
+
+export async function middleware(req: NextRequest) {
+  const session = await getSession();
+  const exist = publicUrlOnly[req.nextUrl.pathname];
+  if (!session.id) {
+    if (!exist) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  } else {
+    if (exist) {
+      return NextResponse.redirect(new URL('/products', req.url))
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
